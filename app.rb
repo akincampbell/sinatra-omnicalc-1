69d73ef193d14661ffede5ae7e9118e127142b48
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-08-03 23:30:49 +0000

    Add some routes

--- app.rb
+++ app.rb
@@ -2,8 +2,32 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f 
+
+  @the_result = @the_num**2 
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f 
+
+  @the_result = Math.sqrt(@the_num)
+
+  erb(:sqrt_results)
+end
+
+get("/payment/new") do
+  #here
 end
